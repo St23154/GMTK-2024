b040f2d374878f00ec9148e4056290f18ef46677
St23154 <e2peyrot@gmail.com>
2024-08-16 22:59:33 +0200

    mini qui marche

--- Assets/Script/ZoomEffect.cs
+++ Assets/Script/ZoomEffect.cs
@@ -3,49 +3,75 @@ using UnityEngine;
 public class ShrinkAnimation : MonoBehaviour
 {
     public bool mini = false;
+    public bool isAnimating = false;
     public float speed = 2f;
     public float posDown = 1;
+    public Animator myCameraAnimator;
     public Vector3 targetScale = new Vector3(0.001f, 0.001f, 0.001f);
     private Vector3 originalScale;
-    private Vector3 originalPos;
     private Vector3 targetPos;
+    private float closeEnough = 0.01f; // Valeur pour définir si on est "suffisamment proche"
 
     void Start()
     {
         originalScale = transform.localScale;
-        originalPos = transform.position;
         targetPos = transform.position;
     }
 
     void Update()
     {
-        if (mini){
-            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, speed * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, targetPos , speed * Time.deltaTime);
-        }else{
-            transform.localScale = Vector3.Lerp(transform.localScale, originalScale, speed * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, targetPos , speed * Time.deltaTime);
-        }
+        if (isAnimating)
+            if (mini)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, targetScale, speed * Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, targetPos) < closeEnough*closeEnough){
+                    transform.position = targetPos;
+                    isAnimating = false;
+                }
+                if (Vector3.Distance(transform.localScale, targetScale) < closeEnough){
+                    transform.localScale = targetScale;
+                }
+            }else{
+                transform.localScale = Vector3.Lerp(transform.localScale, originalScale, speed * Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, targetPos) < closeEnough){
+                    transform.position = targetPos;
+                    isAnimating = false;
+                }
+                if (Vector3.Distance(transform.localScale, originalScale) < closeEnough*closeEnough){
+                    transform.localScale = originalScale;
+                }
+            }
     }
 
     public void Mini()
     {
-        if (mini == true){
+        if (mini == true)
+        {
             Debug.Log("tu est déjà mini");
-        }else{
+        }
+        else
+        {
             mini = true;
             targetPos = new Vector3(transform.position.x, transform.position.y - posDown, transform.position.z);
+            myCameraAnimator.SetTrigger("Zoom");
+            isAnimating = true;
         }
     }
 
     public void Grand()
     {
-        if (mini == false){
+        if (mini == false)
+        {
             Debug.Log("tu est déjà grand");
-        }else{
+        }
+        else
+        {
             mini = false;
             targetPos = new Vector3(transform.position.x, transform.position.y + posDown, transform.position.z);
-        }       
+            myCameraAnimator.SetTrigger("UnZoom");
+            isAnimating = true;
+        }
     }
 }
-
