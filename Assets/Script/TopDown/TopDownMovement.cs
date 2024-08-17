12978790d0b7d51b53026420e1644ffd9c751bf7
St23154 <e2peyrot@gmail.com>
2024-08-17 07:07:45 +0200

    debut top down

---
+++ Assets/Script/TopDown/TopDownMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.velocity = new Vector2(speedX, speedY).normalized * movSpeed;
+    }
+}
