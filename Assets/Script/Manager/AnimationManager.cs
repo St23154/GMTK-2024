f0a249ffe2a6503b4edd30979283cf25632ab52f
St23154 <e2peyrot@gmail.com>
2024-08-17 23:16:06 +0200

    anim en cous

--- Assets/Script/Manager/AnimationManager.cs
+++ Assets/Script/Manager/AnimationManager.cs
@@ -5,7 +5,7 @@ public class AnimationManager : MonoBehaviour
 {
     private Animator myAnimator;
     public int index = 0;
-    private List<string> functionNames = new List<string> { "index1", "index2", "index3", "index4" };
+    private List<string> functionNames = new List<string> { "index1", "index2", "index3", "index4", "index5", "index6", "index7", "index8" };
     public List<GameObject> dialogueBoxes;
 
     void Start()
@@ -34,11 +34,37 @@ public class AnimationManager : MonoBehaviour
         AudioManager.instance.Play("Chaos");
         AudioManager.instance.Stop("Mysterious");
         myAnimator.SetTrigger("2");
-        NextIndex(3f);
+        NextIndex(0f);
     } 
 
     public void index3()
     {
+        dialogueBoxes[1].SetActive(true); 
+    }
 
+    public void index4()
+    {
+        AudioManager.instance.Play("Boss");
+        AudioManager.instance.Stop("Chaos");
+        myAnimator.SetTrigger("4"); 
+        NextIndex(3.6f);
+    }
+
+    public void index5()
+    {
+        dialogueBoxes[2].SetActive(true);
+    }
+
+    public void index6()
+    {
+        myAnimator.SetTrigger("5");
+        NextIndex(22f);
+    }
+
+    public void index7()
+    {
+        AudioManager.instance.Play("Chaos");
+        AudioManager.instance.Stop("Boss");
+        dialogueBoxes[3].SetActive(true);
     }
 }
