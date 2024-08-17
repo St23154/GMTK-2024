35fa3e513cfec604fcc5d26af5437aa12436be71
St23154 <e2peyrot@gmail.com>
2024-08-17 15:54:35 +0200

    sons marchent

--- Assets/Script/Manager/AnimationManager.cs
+++ Assets/Script/Manager/AnimationManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
@@ -7,12 +5,7 @@ public class AnimationManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.instance.Play("Background");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
