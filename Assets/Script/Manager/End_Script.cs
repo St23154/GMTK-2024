0c8253b1c975d6da3c02f03a08c8296ced7fdedf
St23154 <e2peyrot@gmail.com>
2024-08-18 14:40:29 +0200

    3ere scene rteliées

--- Assets/Script/Manager/End_Script.cs
+++ Assets/Script/Manager/End_Script.cs
@@ -6,8 +6,7 @@ public class Next_Level : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Player")){
-            Debug.Log("az");
-            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene("TopDownMainScene");
         }
     }
 }
